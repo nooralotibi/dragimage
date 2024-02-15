54b04234ce908ab3024e5d84088ef8ecd75e38c9
nooreealotibi <nooreealotibi@gmail.com>
2024-02-15 05:30:38 +0300

    Initial Commit

---
+++ dragimage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dragimage
+//
+//  Created by noor alotibi on 05/08/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dragimage/dragimageApp.swift
@@ -0,0 +1,17 @@
+//
+//  dragimageApp.swift
+//  dragimage
+//
+//  Created by noor alotibi on 05/08/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct dragimageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
