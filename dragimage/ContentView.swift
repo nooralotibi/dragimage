9dc38d2b50cf2323807e99f89357b50302e1263c
nooreealotibi <nooreealotibi@gmail.com>
2024-02-15 07:20:53 +0300

    second

--- dragimage/ContentView.swift
+++ dragimage/ContentView.swift
@@ -8,17 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    let charecter = ["Boy"," Girl","boy1"]
+    @State private var imageplac =  Image(systemName: "photo")
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+          
+                            LinearGradient(
+                                gradient: Gradient(colors: [.pink, .blue]),
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                            .opacity(0.8)
+                            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        
+            VStack {
+                Text("Pick profile charecter")
+                    .font(.largeTitle.italic())
+                    .bold()
+                
+                imageplac
+                    .resizable()
+                    .scaledToFit()
+                    .fontWeight(.ultraLight)
+                    .frame(width: 290)
+                    .padding()
+                    .dropDestination(for: Image.self){ items, location in
+                        imageplac = items.first ?? Image(systemName: "photo")
+                        
+                        return true
+                        
+                    }
+                    .padding()
+                ZStack{
+
+                      
+
+                       
+                        (
+                                      LinearGradient(gradient: Gradient(colors: [.pink, .blue]), startPoint: .top,
+                                                     endPoint: .bottom)
+                                        .frame(width: 333,height: 222)
+                                          .cornerRadius(25)
+                                          .shadow(color: .black, radius: 4, x: 4, y: 8)
+                                          .opacity(0.3)
+                                  )
+               
+                        
+                    HStack{
+                        ForEach(charecter, id:\.self){ charecters in
+                            Image(charecters)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 220)
+                                .draggable(Image(charecters))
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
